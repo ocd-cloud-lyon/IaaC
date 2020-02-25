a8fc2cf485545cc0dcfe529f0895ed4b887326e1
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-25 17:47:52 +0100

    Add files via upload

---
+++ AWS_create_EC2_instance.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "test-EC2" {
+  ami                    = "ami-0b248d53709a335ce" #id of desired AMI
+  instance_type          = "t2.small"
+  subnet_id              = "subnet-0c187777047b53659"
+  key_name               = "SMA-KEY"
+  tags = {
+    Name = "eks-worker-node"
+  }
+}
+
+#Configuration des Outputs
+output "public_ip" {
+  value       = "${aws_instance.test-EC2.public_ip}"
+  description = "The private IP address of the main server instance."
+}
+
