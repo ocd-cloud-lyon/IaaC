10caed33ecb5bb85f68e0ddd7bc5fb79dbcbbe0d
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-26 08:53:37 +0100

    Update AWS_create_EC2_instance.tf

--- AWS_create_EC2_instance.tf
+++ AWS_create_EC2_instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-EC2" {
+resource "aws_instance" "worker-node" {
   ami                    = "ami-0b248d53709a335ce" #id of desired AMI
   instance_type          = "t2.small"
   subnet_id              = "subnet-0c187777047b53659"
@@ -10,7 +10,7 @@ resource "aws_instance" "test-EC2" {
 
 #Configuration des Outputs
 output "public_ip" {
-  value       = "${aws_instance.test-EC2.public_ip}"
+  value       = "${aws_instance.worker-node.public_ip}"
   description = "The private IP address of the main server instance."
 }
 
